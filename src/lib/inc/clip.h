2fc511e2bea3f77da30f3a50948e6f57e207f363
Florin Chertes <florin@k2>
2020-04-15 17:29:34 +0200

    Contribution map

--- src/lib/inc/clip.h
+++ src/lib/inc/clip.h
@@ -1,11 +1,17 @@
-#include <string>
 
+#include <unordered_map>
+#include <string>
+#include <mutex>
+#include <memory>
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 class Contribution_basis
 {
 public:
+
+	using Ptr = std::shared_ptr<Contribution_basis>;
+
 	virtual std::string to_string() const = 0;
 
 	virtual std::string name() const { 
@@ -52,3 +58,25 @@ public:
 	T content; 
 };
 
+
+class Contribution_map
+{
+public:
+    constexpr static const auto defaultStorageName = "default";
+
+	using Storage = std::unordered_map<std::string, Contribution_basis::Ptr>;
+
+private:
+
+	void setContributionPtr(const std::string &key, 
+							const Contribution_basis::Ptr value, 
+							const std::string & storageName = defaultStorageName);
+							
+    Contribution_basis::Ptr getContributionPtr(const std::string &storageName, 
+											   const std::string & key) const;
+
+
+	std::unordered_map<std::string, Storage>	_contributions;
+    mutable std::mutex							_mutex;
+
+};
--- src/lib/src/clip.cpp
+++ src/lib/src/clip.cpp
@@ -1,5 +1,11 @@
 #include <clip.h>
 
+#include <iostream>
+#include <exception>
+
+using WriteLock = std::lock_guard<std::mutex>;
+using ReadLock = WriteLock; // Replace by shared_lock in c++17
+
 std::string Contribution_basis::to_string() const
 {
 	const std::string s("Contribution::to_string");
@@ -11,3 +17,25 @@ std::string Contribution_derived::name() const
 	return "name: Contribution_derived"; 
 }
 
+void Contribution_map::setContributionPtr(const std::string &key, 
+										  const Contribution_basis::Ptr value, 
+										  const std::string &storageName)
+{
+    WriteLock lock(_mutex);
+    std::cout << storageName << " " << key;
+    _contributions[storageName][key] = value;
+}
+
+Contribution_basis::Ptr Contribution_map::getContributionPtr(const std::string &storageName, 
+														   const std::string &key) const
+{
+    try {
+        ReadLock lock(_mutex);
+        return  _contributions.at(storageName).at(key);
+
+    } catch(const std::out_of_range &) {
+		std::string msg = std::string("Missing contribution: ") + key + std::string(" in storage: ") + storageName;
+        throw std::range_error(std::string("Missing contribution: ") + key + std::string(" in storage: ") + storageName);
+
+    }
+}
